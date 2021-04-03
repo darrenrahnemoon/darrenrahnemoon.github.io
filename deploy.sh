45cdcb7e832b56f9a4e1ef5d9549fb0101ad47ee
Darren Rahnemoon <darrenrahnemoon@gmail.com>
2021-04-03 11:21:17 -0400

    gh-pages deployment setup

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f https://github.com/darrenrahnemoon/darrenrahnemoon.github.io.git master
+
+cd -
